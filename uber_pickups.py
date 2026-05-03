67fced277bf943c2716d1bbe3025f4a412c00ba3
Chieu Anh Le <genevieve.le19@gmail.com>
2026-05-02 19:15:47 -0700

    Update app title from Ashraf to Genevieve

--- uber_pickups.py
+++ uber_pickups.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Ashraf's App: Uber pickups in NYC")
+st.title("Genevieve's App: Uber pickups in NYC")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
